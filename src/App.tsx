11510457678d34656166ce7d838feabe74283cc8
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-12-29 22:18:15 -0300

    feat: app providers and lib styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { ThemeProvider } from "styled-components";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+import "rsuite/dist/rsuite.min.css";
+
 import { GlobalStyle } from "./styles/globalStyles";
+import { AppProvider } from "./hooks";
+import Theme from "./styles/theme";
+import Loading from "./components/Loading";
+import RoutesComponent from "@/routes/routes.component";
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <h1>Hello World</h1>
-    </>
+    <AppProvider>
+      <ThemeProvider theme={Theme}>
+        <Loading />
+        <GlobalStyle />
+        <RoutesComponent />
+        <ToastContainer theme="colored" />
+      </ThemeProvider>
+    </AppProvider>
   );
 }
 
