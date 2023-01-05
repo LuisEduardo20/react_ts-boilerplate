c6ebf6b018063d44595968364f8f8ad827929819
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-01-05 09:57:32 -0300

    feat: remove input and textarea default border and autocomplete background

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -19,6 +19,15 @@ export const GlobalStyle = createGlobalStyle`
   button {
     cursor: pointer;
   }
+  input:focus, textarea:focus {
+    outline: none;
+  }
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover, 
+  input:-webkit-autofill:focus, 
+  input:-webkit-autofill:active {
+    -webkit-box-shadow: 0 0 0 30px white inset !important;
+  }
   .spinner {
     z-index: 5;
   }
